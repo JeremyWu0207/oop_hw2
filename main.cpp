3a314c62a5ae7e08f7e83eb4ddb9e21f2b7c0d32
JeremyWu0207 <106864061+JeremyWu0207@users.noreply.github.com>
2026-03-12 11:19:25 +0800

    Implement skin shop display and purchase logic

---
+++ main.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+#include "skin.h"
+
+using namespace std;
+
+void display() {
+    cout << "================= skin shop =================" << endl;
+    cout << "C1:" << endl;
+    for (int i = 0; i < 3; i++) {
+        cout << shop[i].getsname();
+        if (shop[i].getsname().is_sold == 1) {
+            cout << "Sold out    ";
+        }
+        else {
+            cout << shop[i].getsname().getprice() << "    ";
+        }
+    }
+    cout << "C2:" << endl;
+    for (int i = 3; i < 6; i++) {
+        cout << shop[i].getsname();
+        if (shop[i].getsname().is_sold == 1) {
+            cout << "Sold out    ";
+        }
+        else {
+            cout << shop[i].getsname().getprice() << "    ";
+        }
+    }
+    cout << "=========================================" <<endl;
+}
+
+int main() {
+    skin shop[6] = {
+        Skin("C1", "S1",100),
+        Skin("C1", "S2",200),
+        Skin("C1", "S3",300),
+        Skin("C2", "S4",400),
+        Skin("C2", "S5",700),
+        Skin("C2", "S6",800);
+    }
+
+        display();
+    int purchase_willing = 0, balance = 0;
+    cin >> "Enter the account amount: " >> amount >> endl;
+    while (1) {
+        cin >> "Enter 1 to buy a skin, 2 to exit: " >> purchase_willing >> endl;
+        if (purchase_willing == 2) break;
+
+        cout << "Account balance: " << balance;
+        cin >> "Enter the character you want to choose: " << #cn << endl;
+        cin >> "Enter the skin you want to purchase: " << #sn << endl;
+
+    }
+
+
+
+
+}
